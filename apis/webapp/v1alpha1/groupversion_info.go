1b9aadb9484883d0c7ad3954d95c8e83b3ef97de
Andreas Fritzler <andreas.fritzler@proton.me>
2022-08-09 09:51:43 +0200

    Improved Makefile

--- apis/webapp/v1alpha1/groupversion_info.go
+++ apis/webapp/v1alpha1/groupversion_info.go
@@ -15,8 +15,8 @@ limitations under the License.
 */
 
 // Package v1alpha1 contains API Schema definitions for the webapp v1alpha1 API group
-//+kubebuilder:object:generate=true
-//+groupName=webapp.afritzler.github.io
+// +kubebuilder:object:generate=true
+// +groupName=webapp.afritzler.github.io
 package v1alpha1
 
 import (
